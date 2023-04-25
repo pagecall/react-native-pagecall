f4af1d2f3e58cf0b12a4328372ebb2ab7699a606
Jaeseong Seo <07js23@gmail.com>
2023-04-25 16:41:25 +0900

    feat: android implementation

--- android/src/main/java/com/pagecall/PagecallPackage.java
+++ android/src/main/java/com/pagecall/PagecallPackage.java
@@ -5,6 +5,7 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -12,11 +13,13 @@ import java.util.List;
 public class PagecallPackage implements ReactPackage {
   @Override
   public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-    return Collections.emptyList();
+    List<NativeModule> modules = new ArrayList<>();
+    modules.add(new PagecallModule(reactContext));
+    return modules;
   }
 
   @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-    return Arrays.<ViewManager>asList(new PagecallViewManager());
+    return Arrays.<ViewManager>asList(new PagecallViewManager(reactContext));
   }
 }
