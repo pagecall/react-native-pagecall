2443527c4408a8e7765318bf899772def7311005
박주렁 <jurung@pplink.net>
2023-04-28 22:50:07 +0900

    refactor: delete unused files

--- ios/PagecallWebviewViewManager.m
+++
@@ -1,8 +0,0 @@
-#import <React/RCTViewManager.h>
-
-@interface RCT_EXTERN_MODULE(PagecallWebviewViewManager, RCTViewManager)
-
-RCT_EXPORT_VIEW_PROPERTY(color, NSString)
-RCT_EXPORT_VIEW_PROPERTY(uri, NSString)
-
-@end
