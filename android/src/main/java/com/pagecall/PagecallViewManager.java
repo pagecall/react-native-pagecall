bcb830b4f443916470ec20af61dc4c71bc3c7fbd
tamini <tamini.kim@gmail.com>
2024-03-11 19:58:41 +0900

    feat(android): support onEvent handler

--- android/src/main/java/com/pagecall/PagecallViewManager.java
+++ android/src/main/java/com/pagecall/PagecallViewManager.java
@@ -22,6 +22,10 @@ import com.facebook.react.uimanager.events.RCTEventEmitter;
 import com.pagecall.PagecallWebView;
 import com.pagecall.TerminationReason;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
 import java.util.Map;
 
 public class PagecallViewManager extends SimpleViewManager<View> implements ActivityEventListener, PagecallWebView.Listener {
@@ -86,6 +90,35 @@ public class PagecallViewManager extends SimpleViewManager<View> implements Acti
   }
 
   // PagecallWebView.Listener implementations
+  private WritableMap jsonObjectToWritableMap(JSONObject jsonObject) throws JSONException {
+    WritableMap writableMap = Arguments.createMap();
+
+    Iterator<String> iterator = jsonObject.keys();
+    while (iterator.hasNext()) {
+      String key = iterator.next();
+      Object value = jsonObject.get(key);
+      if (value instanceof String) {
+        writableMap.putString(key, (String) value);
+      } else if (value instanceof Boolean) {
+        writableMap.putBoolean(key, (Boolean) value);
+      } else if (value instanceof Integer) {
+        writableMap.putInt(key, (Integer) value);
+      } else if (value instanceof Double) {
+        writableMap.putDouble(key, (Double) value);
+      } else if (value instanceof JSONObject) {
+        writableMap.putMap(key, this.jsonObjectToWritableMap((JSONObject) value));
+      }
+    }
+
+    return writableMap;
+  }
+
+  private WritableMap createNativeEvent(String type, JSONObject payload) throws JSONException {
+    WritableMap event = Arguments.createMap();
+    event.putString("type", type);
+    event.putMap("payload", this.jsonObjectToWritableMap(payload));
+    return event;
+  }
   private WritableMap createNativeEvent(String type, String payloadKey, String payloadValue) {
     WritableMap event = Arguments.createMap();
     event.putString("type", type);
@@ -110,6 +143,17 @@ public class PagecallViewManager extends SimpleViewManager<View> implements Acti
     reactContext
       .getJSModule(RCTEventEmitter.class)
       .receiveEvent(webView.getId(), "onNativeEvent", createNativeEvent("message", "message", message));
+  }
+
+  @Override
+  public void onEvent(JSONObject payload) {
+    try {
+      reactContext
+        .getJSModule(RCTEventEmitter.class)
+        .receiveEvent(webView.getId(), "onNativeEvent", createNativeEvent("event", payload));
+    } catch (JSONException error) {
+      Log.e("PagecallViewManager", error.toString());
+    }
   }
 
   @Override
